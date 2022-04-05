b7b153862fdb722d5c8d158f4bb042410ef6aafe
Subhashree naik <subhashree.naik@gmail.com>
2022-04-05 22:43:46 +0530

    UC 3: calculating wages of employee with no of working days

--- EmployeeWage/employeeWage.js
+++ EmployeeWage/employeeWage.js
@@ -4,9 +4,11 @@ const IS_ABSENT=0;
 const IS_PRESENT=1;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 let employeeHour=0;
+let totalEmpHours = 0;
 let empCheck = Math.floor(Math.random()*10)%2;
 let empSalary = Math.floor((Math.random() * 10) % 3);
 if(empCheck == IS_ABSENT){
@@ -32,8 +34,14 @@ function getWorkingHours(empSalary){
 
 empHours = getWorkingHours(empSalary);
 
-//Calculating employee wage
-let empWage = empHours * WAGE_PER_HOUR;
-console.log("Employee wage is $" + empWage);
 
 
+//UC4
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    let empSalary = Math.floor((Math.random() * 10) % 3);
+    totalEmpHours += getWorkingHours(empSalary);
+}
+
+
+let empWage = totalEmpHours * WAGE_PER_HOUR;
+console.log("Employee worked for " + totalEmpHours + " hours and wage is $" + empWage);
