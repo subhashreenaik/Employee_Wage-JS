e6f073cf1eab7d0b1fbdd0aeb532fc4c45efdc9b
Subhashree naik <subhashree.naik@gmail.com>
2022-04-05 22:23:34 +0530

    UC 3: calculating wages of employee with retriving emphour

--- EmployeeWage/employeeWage.js
+++ EmployeeWage/employeeWage.js
@@ -1,11 +1,12 @@
 
-// UC:2
+
 const IS_ABSENT=0;
 const IS_PRESENT=1;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20
 
+let employeeHour=0;
 let empCheck = Math.floor(Math.random()*10)%2;
 let empSalary = Math.floor((Math.random() * 10) % 3);
 if(empCheck == IS_ABSENT){
@@ -15,21 +16,24 @@ else{
     console.log("Employee is present")
 }
 
-//Switch case to calculate wage according to random values generated
-function calculate(empSalary){
-switch(empSalary) {
-    case 0:
-        console.log("Employee wage is 0");
-        break;
 
-    case 1:
-        console.log("Employee wage is " + (PART_TIME_HOURS * WAGE_PER_HOUR));
-        break;
+function getWorkingHours(empSalary){
+    switch(empSalary) {
+        case 0:
+            return 0;
 
-    case 2:
-        console.log("Employee wage is " + (FULL_TIME_HOURS * WAGE_PER_HOUR));
-        break;
+        case 1:
+            return PART_TIME_HOURS;
+    
+        case 2:
+            return FULL_TIME_HOURS;
+    }
 }
-}
-console.log(calculate(empSalary));
+
+empHours = getWorkingHours(empSalary);
+
+//Calculating employee wage
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Employee wage is $" + empWage);
+
 
